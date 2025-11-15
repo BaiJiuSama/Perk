c236f6e131415afdd5dfcb6dd9bb2ae1bda3bd81
Irina <ItsIrina_@outlook.com>
2025-11-15 16:45:23 +0800

    feat: ExamplePerk 属性操控API使用高版本Attribute API

--- src/main/kotlin/cn/irina/perk/perks/impl/ExamplePerk.kt
+++ src/main/kotlin/cn/irina/perk/perks/impl/ExamplePerk.kt
@@ -2,6 +2,7 @@ package cn.irina.perk.perks.impl
 
 import cn.irina.perk.model.Perk
 import cn.irina.perk.util.CC
+import org.bukkit.attribute.Attribute
 import org.bukkit.entity.Player
 
 /**
@@ -14,19 +15,15 @@ class ExamplePerk: Perk() {
     
     override fun name(): String = "示例天赋"
     
-    override fun description(): String = "这是一个示例天赋, 会增加玩家的血量属性"
+    override fun description(): String = "这是一个示例天赋, 会增加玩家的最大血量属性"
     
     override fun execute(player: Player) {
-        val oldHealth = player.health
-        val newHealth = oldHealth + 4
-        player.health = newHealth
-        player.sendMessage(CC.color("示例天赋触发, 原先血量: $oldHealth 当前血量: ${player.health}"))
+        player.getAttribute(Attribute.GENERIC_MAX_HEALTH)!!.baseValue += 4
+        player.sendMessage(CC.color("&e示例天赋触发, 当前血量: ${player.getAttribute(Attribute.GENERIC_MAX_HEALTH)!!.baseValue}"))
     }
     
     override fun cancel(player: Player) {
-        val oldHealth = player.health
-        val newHealth = oldHealth - 4
-        player.health = newHealth
-        player.sendMessage(CC.color("示例天赋触发, 原先血量: $oldHealth 当前血量: ${player.health}"))
+        player.getAttribute(Attribute.GENERIC_MAX_HEALTH)!!.baseValue -= 4
+        player.sendMessage(CC.color("&e示例天赋触发, 当前血量: ${player.getAttribute(Attribute.GENERIC_MAX_HEALTH)!!.baseValue}"))
     }
 }
